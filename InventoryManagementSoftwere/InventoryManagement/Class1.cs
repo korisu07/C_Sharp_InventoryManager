5bba5675d28d1092f3374cb76c9b41b7a36ec6e9
korisu07 <Hallo.Dreamy@gmail.com>
2021-07-06 16:47:34 +0900

    Update.

---
+++ InventoryManagementSoftwere/InventoryManagement/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+class Test1
+{
+	static void Main()
+	{
+		string sql = "SELECT id,name,romaji FROM SYAIN";
+		try
+		{
+			using (SqlConnection conn = new SqlConnection())
+			{
+				conn.ConnectionString =
+				@"Data Source=localhost\SQLEXPRESS;" +
+				"Initial Catalog=testDB1;" +
+				 "Integrated Security=SSPI;";
+
+				conn.Open();
+				using (SqlCommand cmd = new SqlCommand(sql))
+				{
+					cmd.Connection = conn;
+					cmd.CommandType = CommandType.Text;
+					using (SqlDataReader reader = cmd.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							Console.WriteLine(reader["id"] + ":"
+											+ reader["name"] + ":"
+											+ reader["romaji"]);
+						}
+					}
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message.ToString());
+		}
+	}
+}
